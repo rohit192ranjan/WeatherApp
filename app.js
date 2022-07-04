f13150970f8ddb33c428713eab7470ce396c6f12
Rohit <rohit192ranjan@gmail.com>
2022-07-04 21:21:49 +0530

    v1.1

---
+++ app.js
@@ -0,0 +1,45 @@
+const ejs = require('ejs');
+const express = require('express');
+const https = require('https');
+const bodyParser = require("body-parser");
+const app = express();
+
+var query = "";
+var temp="";
+var imgURL="";
+var weatherdescription="";
+var status="";
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", function(req, res){
+    res.render("weather",{query:query, temp:temp, imgURL:imgURL, weatherdescription:weatherdescription, status:status});
+});
+
+app.post("/", function(req,res){
+    query = req.body.cityname;
+    const apikey = "231bc5e82792bcd4e8afc37f1e65d98e";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units=metric&appid="+apikey;
+    https.get(url, function(response){
+        status = response.statusCode;
+        console.log(status);
+        response.on("data",function(data){
+            const weatherdata = JSON.parse(data);
+            if(status=="200"){
+            temp = weatherdata.main.temp;
+            weatherdescription = weatherdata.weather[0].description;
+            const icon = weatherdata.weather[0].icon;
+            imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";}
+            res.redirect("/");
+        })
+    })
+});
+
+app.listen(3000, function(){
+    console.log('Server running on port 3000');
+});

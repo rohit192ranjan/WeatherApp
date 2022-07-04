7f469f01d07f332122e074555fdd55cd2791ccdc
Rohit <rohit192ranjan@gmail.com>
2022-07-04 21:55:30 +0530

    v1.1.1

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.post("/", function(req,res){
     })
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Server running on port 3000');
 });

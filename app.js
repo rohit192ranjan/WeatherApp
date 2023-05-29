82e34f303bc3748ef3ab0af22097bc2ecaa86224
Rohit <rohit192ranjan@gmail.com>
2023-05-29 11:08:45 +0530

    UI & Feature upgrade v2.0

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var temp="";
 var imgURL="";
 var weatherdescription="";
 var status="";
+var minTemp = "";
+var maxTemp = "";
+var feelsLike = "";
+var pressure = "";
+var humidity = "";
+var wind = "";
 
 app.set('view engine', 'ejs');
 
@@ -19,7 +25,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req, res){
-    res.render("weather",{query:query, temp:temp, imgURL:imgURL, weatherdescription:weatherdescription, status:status});
+    res.render("weather",{query:query, temp:temp, imgURL:imgURL, weatherdescription:weatherdescription, status:status, mint: minTemp, maxt: maxTemp, flike: feelsLike, press: pressure, hum: humidity, wspeed: wind});
 });
 
 app.post("/", function(req,res){
@@ -28,14 +34,20 @@ app.post("/", function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units=metric&appid="+apikey;
     https.get(url, function(response){
         status = response.statusCode;
-        console.log(status);
+        //console.log(status);
         response.on("data",function(data){
             const weatherdata = JSON.parse(data);
             if(status=="200"){
             temp = weatherdata.main.temp;
             weatherdescription = weatherdata.weather[0].description;
             const icon = weatherdata.weather[0].icon;
-            imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";}
+            imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            minTemp = weatherdata.main.temp_min + " °C";
+            maxTemp = weatherdata.main.temp_max + " °C";
+            feelsLike = weatherdata.main.feels_like + " °C";
+            pressure = weatherdata.main.pressure + " hPa";
+            humidity = weatherdata.main.humidity + " %";
+            wind = weatherdata.wind.speed + " m/s";}
             res.redirect("/");
         })
     })
